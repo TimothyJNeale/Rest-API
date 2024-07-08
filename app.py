10db9d4720742068aefcfe7816c3a4120722eda1
TimothyJNeale <sixhillsai@icloud.com>
2024-07-08 12:00:38 +0100

    Added changes endpoints for users

--- app.py
+++ app.py
@@ -7,11 +7,11 @@ from flask_jwt_extended import JWTManager
 from dotenv import load_dotenv
 
 from db import db
-import models
 
 from resources.item import blp as ItemBlueprint
 from resources.store import blp as StoreBlueprint
 from resources.tag import blp as TagBlueprint
+from resources.user import blp as UserBlueprint
 
 def create_app(db_url=None):
     app = Flask(__name__)
@@ -41,5 +41,6 @@ def create_app(db_url=None):
     api.register_blueprint(ItemBlueprint)
     api.register_blueprint(StoreBlueprint)
     api.register_blueprint(TagBlueprint)
+    api.register_blueprint(UserBlueprint)
 
     return app
